7adf49040aa2649fb160a8b1a7837adbdd2b5bae
puria kazemieh <pkazemieh@tosantechno.com>
2025-08-09 15:51:04 +0330

    initial commit

---
+++ src/main/kotlin/com/kazemieh/divar/DivarApplication.kt
@@ -0,0 +1,11 @@
+package com.kazemieh.divar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DivarApplication
+
+fun main(args: Array<String>) {
+    runApplication<DivarApplication>(*args)
+}
---
+++ src/test/kotlin/com/kazemieh/divar/DivarApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kazemieh.divar
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DivarApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
